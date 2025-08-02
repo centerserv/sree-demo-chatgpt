4f631516f1663a5dff8a7d5b65e49f125e09ba7a
centerserv <amorin@centerserv.com>
2025-08-02 11:15:24 -0400

    Add files via upload

--- trust_update.py
+++ trust_update.py
@@ -1,28 +1,33 @@
-import numpy as np
-import optuna
-
-def update_trust(data, labels, pattern, presence, permanence, logic, iterations=10, alpha=0.1, beta=0.4, gamma=0.1, delta=0.2):
-    def run_trial(trial):
-        a = trial.suggest_float("alpha", 0.05, 0.2)
-        b = trial.suggest_float("beta", 0.3, 0.5)
-        g = trial.suggest_float("gamma", 0.05, 0.2)
-        d = trial.suggest_float("delta", 0.1, 0.3)
-        pattern.fit(X_train, y_train)
-        preds, probs, _ = pattern.validate(X_test, y_test)
-        T = 0.5
-        for _ in range(iterations):
-            V_q = presence.validate(probs)
-            V_b = permanence.validate(preds, probs)
-            V_l = logic.validate(preds, X_test)
-            V_t = b*V_b + (1-b-d)*V_q + d*V_l
-            T = a*V_t + (1-a)*T
-            weighted = probs * V_q[:,None] * V_b[:,None] * V_l[:,None]
-            preds = np.argmax(weighted, axis=1)
-        return np.mean(T)
-    study = optuna.create_study(direction="maximize")
-    study.optimize(run_trial, n_trials=20)
-    params = study.best_params
-    return params
-
-# Note: For demo simplicity, you can call this inside main to get best params.
+--- a/trust_update.py
++++ b/trust_update.py
+@@
+-def update_trust(data, labels, pattern, presence, permanence, logic, iterations=10):
+-    def run_trial(trial):
+-        a = trial.suggest_float("alpha", 0.05, 0.2)
+-        b = trial.suggest_float("beta", 0.3, 0.5)
+-        g = trial.suggest_float("gamma", 0.05, 0.2)
+-        d = trial.suggest_float("delta", 0.1, 0.3)
+-        pattern.fit(X_train, y_train)
+-        preds, probs, _ = pattern.validate(X_test, y_test)
++from sklearn.model_selection import train_test_split
++
++def update_trust(data, labels, pattern, presence, permanence, logic, iterations=10):
++    # split once, outside the trials
++    X_train, X_test, y_train, y_test = train_test_split(
++        data, labels, test_size=0.2, random_state=42
++    )
++
++    def run_trial(trial):
++        a = trial.suggest_float("alpha", 0.05, 0.2)
++        b = trial.suggest_float("beta", 0.3, 0.5)
++        g = trial.suggest_float("gamma", 0.05, 0.2)
++        d = trial.suggest_float("delta", 0.1, 0.3)
++        # now these exist:
++        pattern.fit(X_train, y_train)
++        preds, probs, _ = pattern.validate(X_test, y_test)
+@@
+-    study = optuna.create_study(direction="maximize")
++    study = optuna.create_study(direction="maximize")
+     study.optimize(run_trial, n_trials=20)
+     return study.best_params
 
